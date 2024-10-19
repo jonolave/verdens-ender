098fc917e065384287254c6f8e741aa1a3f83ad6
Jon Olav Eikenes <jonolav.eikenes@gmail.com>
2024-10-19 16:29:24 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { slide, fly } from 'svelte/transition';
+
+  let y;
+  $: console.log(y);
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<main>
+    <div id="navbar" class:show={y >= 80}>
+      <p>Navbar {y}</p>
+    </div>
+
+  <p>scroll down..</p>
+
+</main>
+
+<style>
+  main {
+    height: 200vh;
+  }
+
+  #navbar {
+    position: fixed;
+    top: -100px;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+  }
+
+  div#navbar.show {
+    top: 0;
+  }
+</style>
